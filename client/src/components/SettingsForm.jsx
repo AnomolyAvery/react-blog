d915d30f427f114b2f20e89be143846fdb16db84
“AnomolyAvery” <ashawsolutions@gmail.com>
2022-06-10 00:38:47 -0400

    Settings dark mode update

--- client/src/components/SettingsForm.jsx
+++ client/src/components/SettingsForm.jsx
@@ -24,31 +24,31 @@ const SettingsForm = () => {
     return (
         <form
             onSubmit={handleSubmit}
-            className="space-y-8 divide-y divide-gray-200"
+            className="space-y-8 divide-y divide-gray-200 dark:divide-neutral-700"
         >
-            <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+            <div className="space-y-8 divide-y divide-gray-200 dark:divide-neutral-700 sm:space-y-5">
                 <div>
                     <div>
-                        <h3 className="text-lg leading-6 font-medium text-gray-900">
+                        <h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">
                             Profile
                         </h3>
-                        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                        <p className="mt-1 max-w-2xl text-sm text-gray-500 dark:text-gray-300">
                             This information will be displayed publicly so be
                             careful what you share.
                         </p>
                     </div>
 
                     <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
-                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 dark:sm:border-gray-700 sm:pt-5">
                             <label
                                 htmlFor="username"
-                                className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                                className="block text-sm font-medium text-gray-700 dark:text-gray-300 sm:mt-px sm:pt-2"
                             >
                                 Id
                             </label>
                             <div className="mt-1 sm:mt-0 sm:col-span-2">
                                 <div className="max-w-lg flex rounded-md shadow-sm">
-                                    <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
+                                    <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 dark:bg-neutral-700 dark:text-gray-300 text-gray-500 sm:text-sm">
                                         theblog.gg/users/
                                     </span>
                                     <input
@@ -56,7 +56,7 @@ const SettingsForm = () => {
                                         name="username"
                                         id="username"
                                         autoComplete="username"
-                                        className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
+                                        className="dark:bg-neutral-800 dark:text-white flex-1 block w-full focus:ring-blue-500 focus:border-blue-500 min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
                                         disabled
                                         value={user.id}
                                     />
@@ -64,10 +64,10 @@ const SettingsForm = () => {
                             </div>
                         </div>
 
-                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 dark:sm:border-neutral-700 sm:pt-5">
                             <label
                                 htmlFor="first-name"
-                                className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                                className="block text-sm font-medium text-gray-700 dark:text-gray-300 sm:mt-px sm:pt-2"
                             >
                                 Name
                             </label>
@@ -77,17 +77,17 @@ const SettingsForm = () => {
                                     name="first-name"
                                     id="first-name"
                                     autoComplete="given-name"
-                                    className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                                    className="dark:bg-neutral-800 dark:text-white max-w-lg block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
                                     value={name}
                                     onChange={(e) => setName(e.target.value)}
                                 />
                             </div>
                         </div>
 
-                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 dark:sm:border-neutral-700 sm:pt-5">
                             <label
                                 htmlFor="about"
-                                className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                                className="block text-sm font-medium text-gray-700 dark:text-gray-300 sm:mt-px sm:pt-2"
                             >
                                 Biography
                             </label>
@@ -96,7 +96,7 @@ const SettingsForm = () => {
                                     id="about"
                                     name="about"
                                     rows={3}
-                                    className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
+                                    className="dark:bg-neutral-800 dark:text-white max-w-lg shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md"
                                     value={biography}
                                     onChange={(e) =>
                                         setBiography(e.target.value)
@@ -115,7 +115,7 @@ const SettingsForm = () => {
                 <div className="flex justify-end">
                     <button
                         type="submit"
-                        className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                     >
                         Save
                     </button>
